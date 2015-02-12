be813e292d4a5c793b89cf9a3bfbb2de2a8dc63d
Zontza <zontzor@gmail.com>
2015-02-12 11:36:51 +0000

    Cleaned up code

--- Queue/QueueTest.java
+++ Queue/QueueTest.java
@@ -4,103 +4,98 @@
 class Queue {
 
 	// nested class: nodes are local to the queue
-    private class Node {
-        int data;
-        Node next;
-    }
+	private class Node {
+		int data;
+		Node next;
+	}
 
-    Node z; // z is an initial node
-    Node head; // head is a node representing the TOP of the queue
-    Node tail; // tail is a node representing the BOTTOM of the queue
+	Node z; // z is an initial node
+	Node head; // head is a node representing the TOP of the queue
+	Node tail; // tail is a node representing the BOTTOM of the queue
 
-    // constructor initialises nodes
-    public Queue() {
-        z = new Node(); 
-        z.next = z; // z.next points to z, which at this time is the only node
-        head = z;  	// head of the queue contains z, which again is the only node at this time
-        tail = null; // tail will always be null (a sentinel) that acts as a terminator
-    }
-
-
-  public void display() {
-    System.out.println("\nThe queue values are:\n");
-
-    Node temp = head;
-    while( temp != temp.next) {
-        System.out.print( temp.data + "  ");
-        temp = temp.next;
-    }
-    System.out.println("\n");
-  }
+	// constructor initialises nodes
+	public Queue() {
+		z = new Node(); 
+		z.next = z; // z.next points to z, which at this time is the only node
+		head = z;  	// head of the queue contains z, which again is the only node at this time
+		tail = null; // tail will always be null (a sentinel) that acts as a terminator
+	}
 
 
-  public void enQueue(int x) {
-    Node temp; // temp is our new node
+	public void display() {
+	System.out.println("\nThe queue values are:\n");
 
-    temp = new Node();
-    temp.data = x; // our new nodes data attribute holds the number we want to store
-    temp.next = z; // in a queue, the newest member is at the back so next is node is z, the initial node
-
-    if(head == z)    // case of empty list
-        head = temp;
-    else                // case of list not empty
-        tail.next = temp;
-
-    tail = temp;        // new node is now at the tail
-  }
+	Node temp = head;
+	while( temp != temp.next) {
+		System.out.print( temp.data + "  ");
+		temp = temp.next;
+	}
+	
+	System.out.println("\n");
+	}
 
 
-  // assume the queue is non-empty when this method is called
-  public int deQueue() {
-    int firstItem = head.data;
-          
-	head = head.next;  // The previous second item is now first.
-	if (head == null) {
-		// The queue has become empty.  The Node that was
-		// deleted was the tail as well as the head of the
-		// list, so now there is no tail.  (Actually, the
-		// class would work fine without this step.)
-		tail = null;
-	} 
+	public void enQueue(int x) {
+	Node temp; // temp is our new node
 
-	return firstItem;
-  }
+	temp = new Node();
+	temp.data = x; // our new nodes data attribute holds the number we want to store
+	temp.next = z; // in a queue, the newest member is at the back so next is node is z, the initial node
+
+	if(head == z)    // case of empty list
+		head = temp;
+		else                // case of list not empty
+		tail.next = temp;
+
+		tail = temp;        // new node is now at the tail
+	}
 
 
-  /*public boolean isEmpty() {
-    do yourself
-  }*/
+	public int deQueue() {
+		int firstItem = head.data;
+
+		head = head.next;  // The previous second item is now first.
+		if (head == null) {
+			// The queue has become empty.  The Node that was
+			// deleted was the tail as well as the head of the
+			// list, so now there is no tail.  (Actually, the
+			// class would work fine without this step.)
+			tail = null;
+		} 
+
+		return firstItem;
+	}
+
+
+	public boolean isEmpty() {
+		return (head == tail);
+	}
 
 } // end of Queue class
 
 
 
 class QueueTest {
-  
-  // try out the ADT Queue using static allocation
-  public static void main(String[] arg) {
 
-    Queue q = new Queue();
+	// try out the ADT Queue using static allocation
+	public static void main(String[] arg) {
 
-    System.out.println("Inserting ints from 9 to 1 into queue gives:\n");
-    for (int i = 9; i > 0; --i) {
-       q.enQueue(i);
-    }
+		Queue q = new Queue();
 
-    q.display();
+		System.out.println("Inserting ints from 9 to 1 into queue gives:\n");
+		for (int i = 9; i > 0; --i) {
+			q.enQueue(i);
+		}
 
-    q.deQueue();
+		q.display();
 
-    q.display();
+		if( ! q.isEmpty()) {
+			System.out.println("Deleting value from queue: " + q.deQueue() + "\n");
 
-   /* if( ! q.isEmpty())
-        System.out.println("Deleting value from queue " + q.deQueue() + "\n");
-
-    System.out.println("Adding value to queue " + 27 + "\n");
-    q.enQueue(27);
-    q.display();
-*/
-  }
-
+			System.out.println("Adding value to queue: " + 27 + "\n");
+			q.enQueue(27);
+			q.display();
+		}
+	}
 } //end of Test class
 
