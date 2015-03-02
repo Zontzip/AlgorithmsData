76532eae1a3dcd97f8862bf05161c5a3e32c570f
Zontzor <alex.kiernan@mydit.ie>
2015-03-02 22:59:27 +0000

    Removed file IO nullpointer

--- DepthFirst/GraphMatrix.java
+++ DepthFirst/GraphMatrix.java
@@ -16,36 +16,29 @@ class GraphMatrix
     private int[] visited;
     private int id;
    
-    
-    
     // default constructor
     public GraphMatrix(String graphFile)  throws IOException
     {
         int u, v;
         int e, wgt;
 
-        // StreamReader reader = new StreamReader(graphFile);
 		FileReader fr = new FileReader(graphFile);
-		BufferedReader reader = new BufferedReader(fr);
-	   
-        // char[] splits = new char[] { ' ', ',', '\t'};     
-        // string line = reader.ReadLine();
-		// string[] parts = line.Split(splits, StringSplitOptions.RemoveEmptyEntries);
+		BufferedReader reader = new BufferedReader(fr); // efficient
         
-        String splits = " +";  // multiple whitespace as delimiter		   
-		String line = reader.readLine();       
+        String splits = "\\s+";  // multiple whitespaces as delimiter		   
+		String line = reader.readLine();    
+		System.out.println(line); 
+		System.out.println("Splits: " + splits);  
         String[] parts = line.split(splits);
         System.out.println("Parts[] = " + parts[0] + " " + parts[1]);
 		
         // find out number of vertices and edges
-        //V = int.Parse(parts[0]);
-        //E = int.Parse(parts[1]);
-        
-		V = Integer.parseInt(parts[0]);
+        V = Integer.parseInt(parts[0]);
         E = Integer.parseInt(parts[1]);
+        System.out.println("Vertices: " + V);
+        System.out.println("Edges: " + E);
 
         // create adjacency matrix, initialised to 0's
-        //adj = new int[V+1, V+1];
         adj = new int[V+1][V+1];        
         
         visited = new int[V+1];
@@ -53,7 +46,7 @@ class GraphMatrix
        // read the edges
         System.out.println("Reading edges from text file");
         for(e = 1; e <= E; ++e)
-       {
+        {
             line = reader.readLine();
             parts = line.split(splits);
             u = Integer.parseInt(parts[0]);
@@ -63,7 +56,7 @@ class GraphMatrix
             System.out.println("Edge " + toChar(u) + "--(" + wgt + ")--" + toChar(v));    
             // write code to put edge into adjacency matrix     
             // missing code here            
-       }	       
+    	}	       
     }
 
 	// convert vertex into char for pretty printing
@@ -71,12 +64,13 @@ class GraphMatrix
     {  
         return (char)(u + 64);
     }
+    
 	
     // method to display the graph representation
     public void display() {
         int u,v;
         
-        for(v=1; v<=V; ++v){
+        for(v=1; v<=V; ++v) {
             System.out.print("\nadj[" + v + "] = ");
             for(u=1; u<=V; ++u) 
                 System.out.print("  " + adj[u][v]);
@@ -84,27 +78,22 @@ class GraphMatrix
         System.out.println("");
     }
 
-
     // method to initialise Depth First Traversal of Graph
     public void DF( int s) 
     {
-        id = 0;
-        
         
     }
 
-
-    // Recursive Depth First Traversal for adjacency matrix
+	// Recursive Depth First Traversal for adjacency matrix
     private void dfVisit( int prev, int v)
     {
 
     }
 
-
     public static void main(String[] args) throws IOException
     {
         int s = 4;
-        String fname = "wGraph3.txt";               
+        String fname = "graph.txt";               
 
         GraphMatrix g = new GraphMatrix(fname);
        
